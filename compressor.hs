8d647c044589f91ce296a80656326cacf3cd09b8
Alexander Ekdahl <alexander@noviumdesign.se>
2013-01-01 16:55:38 +0100

    comments

--- compressor.hs
+++ compressor.hs
@@ -1,5 +1,7 @@
 type CompressedData = ([String], [Int])
 
+-- > compress "This specification is the specification for a specification"
+-- (["This","specification","is","the","for","a"],[0,1,2,3,1,4,5,1])
 compress :: String -> CompressedData
 compress s = foldl f ([], []) (words s)
   where
@@ -10,5 +12,7 @@ compress s = foldl f ([], []) (words s)
       | x == y    = i
       | otherwise = g x ys (i + 1)
 
+-- > decompress (["This","specification","is","the","for","a"],[0,1,2,3,1,4,5,1])
+-- "This specification is the specification for a specification"
 decompress :: CompressedData -> String
 decompress (u, i) = unwords $ map (u !!) i
